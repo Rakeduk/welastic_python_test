7a689e2ddee3582fb0427368d0b164b0088eafe3
Rakeduk <mlodydudek@gmail.com>
2022-06-15 16:56:49 +0200

    Insert data into tables

--- exc_1_database.py
+++ exc_1_database.py
@@ -30,5 +30,22 @@ c.execute('''CREATE TABLE IF NOT EXISTS film(
 	langauage VARCHAR(64)
 	) ''')
 
+# Add data to tables
+c.execute('''INSERT INTO client VALUES
+	(1, 'Patryk', 'Kowalski', '52-856'),
+	(2, 'Maria', 'Kowalska', '52-856'),
+	(3, 'Patryk', 'Nowak', '62-718')
+	 ''')
+
+c.execute(''' INSERT INTO address VALUES
+	(1, 'Poland', 'Warszawa', 'Miejska', 26, '52-856'),
+	(2, 'Poland', 'Poznań', 'Owcza', 75, '62-718')
+	''')
+
+c.execute('''INSERT INTO film VALUES
+	(1, 'Inside Man', 'Thriller', 129, 'english'),
+	(2, 'The Sandman', 'Horror', 112, 'english')
+ 	''')
+
 conn.commit()
 
