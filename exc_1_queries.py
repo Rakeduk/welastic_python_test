7f7d2ff99ee54b4dfdb402deacd60f0616b81016
Rakeduk <mlodydudek@gmail.com>
2022-06-15 18:44:13 +0200

    Return query results in JSON

--- exc_1_queries.py
+++ exc_1_queries.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 
 # Creating database
 conn = sqlite3.connect('Baza_pyth')
@@ -6,24 +7,41 @@ conn = sqlite3.connect('Baza_pyth')
 c = conn.cursor()
 
 # Wyświetlić klientów którzy mieszkają kod zamieszkania 52-856
-c.execute("SELECT * FROM client WHERE zip_code LIKE '52-856'")
+c.execute('''SELECT JSON_OBJECT(
+	'id', id,
+	'name', name,
+	'last_name', last_name,
+	'zip_code', zip_code
+	)
+	FROM client WHERE zip_code LIKE '52-856' ''')
 
 output = c.fetchall()
-for row in output:
-	print(row)
+print(output)
 
 # Wyświetlić adres pod którym mieszka Patryk Nowak
-c.execute('''SELECT * FROM address WHERE zip_code IN 
-	(SELECT zip_code FROM client WHERE name LIKE 'Patryk' AND last_name LIKE 'Nowak')
-	''')
+c.execute('''SELECT JSON_OBJECT(
+	'id', id,
+	'country', country,
+	'city', city,
+	'street', street,
+	'number', number,
+	'zip_code', zip_code)
+	FROM address WHERE zip_code IN 
+	(SELECT zip_code FROM client WHERE name LIKE 'Patryk' AND last_name LIKE 'Nowak') ''')
 
 output = c.fetchall()
-for row in output:
-	print(row)
+print(output)
 
 # Wyświetlić film z kategorii thriller w języku angielskim
-c.execute("SELECT * FROM film WHERE category LIKE 'Thriller' AND language LIKE 'english'")
+c.execute('''SELECT JSON_OBJECT(
+	'id', id,
+	'name', name,
+	'category', category,
+	'length', length,
+	'language', language)
+	FROM film WHERE category LIKE 'Thriller' AND language LIKE 'english' ''')
 
 output = c.fetchall()
-for row in output:
-	print(row)
+print(output)
+
+
