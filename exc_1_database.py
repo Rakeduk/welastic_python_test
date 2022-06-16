c685e199799677f359ccf740733c33b469b3caaf
Rakeduk <mlodydudek@gmail.com>
2022-06-16 15:35:37 +0200

    Complete database part, fix style

--- exc_1_database.py
+++ exc_1_database.py
@@ -1,49 +1,57 @@
 import sqlite3
 
-# Creating database
-conn = sqlite3.connect('Baza_pyth')
 
-c = conn.cursor()
+def create_tables(c, conn):
+    c.execute('''CREATE TABLE IF NOT EXISTS client(
+        id INTEGER,
+        name VARCHAR(64),
+        last_name VARCHAR(64),
+        zip_code VARCHAR(6)
+        ) ''')
 
-# Create tables
-c.execute('''CREATE TABLE IF NOT EXISTS client(
-    id INTEGER,
-    name VARCHAR(64),
-    last_name VARCHAR(64),
-    zip_code VARCHAR(6)
-    ) ''')
+    c.execute('''CREATE TABLE IF NOT EXISTS address(
+        id INTEGER,
+        country VARCHAR(64),
+        city VARCHAR(64),
+        street VARCHAR(64),
+        number INTEGER,
+        zip_code VARCHAR(6)
+        ) ''')
 
-c.execute('''CREATE TABLE IF NOT EXISTS address(
-    id INTEGER,
-    country VARCHAR(64),
-    city VARCHAR(64),
-    street VARCHAR(64),
-    number INTEGER,
-    zip_code VARCHAR(6)
-    ) ''')
+    c.execute('''CREATE TABLE IF NOT EXISTS film(
+        id INTEGER,
+        name VARCHAR(255),
+        category VARCHAR(64),
+        length INTEGER,
+        language VARCHAR(64)
+        ) ''')
+    conn.commit()
 
-c.execute('''CREATE TABLE IF NOT EXISTS film(
-    id INTEGER,
-    name VARCHAR(255),
-    category VARCHAR(64),
-    length INTEGER,
-    language VARCHAR(64)
-    ) ''')
 
-# Add data to tables
-c.execute('''INSERT INTO client VALUES
-    (1, 'Patryk', 'Kowalski', '52-856'),
-    (2, 'Maria', 'Kowalska', '52-856'),
-    (3, 'Patryk', 'Nowak', '62-718')
-     ''')
+def insert_data(c, conn):
+    c.execute('''INSERT INTO client VALUES
+        (1, 'Patryk', 'Kowalski', '52-856'),
+        (2, 'Maria', 'Kowalska', '52-856'),
+        (3, 'Patryk', 'Nowak', '62-718')
+        ''')
 
-c.execute(''' INSERT INTO address VALUES
-    (1, 'Poland', 'Warszawa', 'Miejska', 26, '52-856'),
-    (2, 'Poland', 'Poznań', 'Owcza', 75, '62-718')
-    ''')
+    c.execute(''' INSERT INTO address VALUES
+        (1, 'Poland', 'Warszawa', 'Miejska', 26, '52-856'),
+        (2, 'Poland', 'Poznań', 'Owcza', 75, '62-718')
+        ''')
 
-c.execute('''INSERT INTO film VALUES
-    (1, 'Inside Man', 'Thriller', 129, 'english'),
-    (2, 'The Sandman', 'Horror', 112, 'english')
-     ''')
-conn.commit()
+    c.execute('''INSERT INTO film VALUES
+        (1, 'Inside Man', 'Thriller', 129, 'english'),
+        (2, 'The Sandman', 'Horror', 112, 'english')
+        ''')
+    conn.commit()
+
+
+def main():
+    conn = sqlite3.connect('Baza_pyth')
+    c = conn.cursor()
+    create_tables(c, conn)
+    insert_data(c, conn)
+
+if __name__ == "__main__":
+    main()
--- exc_1_queries.py
+++ exc_1_queries.py
@@ -1,53 +1,51 @@
 import sqlite3
 
-# Creating database
-conn = sqlite3.connect('Baza_pyth')
 
-c = conn.cursor()
+def queries(c):
+    c.execute('''SELECT JSON_OBJECT(
+        'id', id,
+        'name', name,
+        'last_name', last_name,
+        'zip_code', zip_code
+        )
+        FROM client WHERE zip_code LIKE '52-856' ''')
+    print(c.fetchall())
 
-# Wyświetlić klientów którzy mieszkają kod zamieszkania 52-856
-c.execute('''SELECT JSON_OBJECT(
-    'id', id,
-    'name', name,
-    'last_name', last_name,
-    'zip_code', zip_code
-    )
-    FROM client WHERE zip_code LIKE '52-856' ''')
+    c.execute('''SELECT JSON_OBJECT(
+        'id', id,
+        'country', country,
+        'city', city,
+        'street', street,
+        'number', number,
+        'zip_code', zip_code)
+        FROM address WHERE zip_code IN
+            (SELECT zip_code FROM client WHERE
+             name LIKE 'Patryk' AND last_name LIKE 'Nowak') ''')
+    print(c.fetchall())
 
-output = c.fetchall()
-print(output)
+    c.execute('''SELECT JSON_OBJECT(
+        'id', id,
+        'name', name,
+        'category', category,
+        'length', length,
+        'language', language)
+        FROM film WHERE category LIKE 'Thriller' AND language LIKE 'english' ''')
+    print(c.fetchall())
 
-# Wyświetlić adres pod którym mieszka Patryk Nowak
-c.execute('''SELECT JSON_OBJECT(
-    'id', id,
-    'country', country,
-    'city', city,
-    'street', street,
-    'number', number,
-    'zip_code', zip_code)
-    FROM address WHERE zip_code IN
-    (SELECT zip_code FROM client WHERE
-    name LIKE 'Patryk' AND last_name LIKE 'Nowak') ''')
 
-output = c.fetchall()
-print(output)
+def modify(c, conn):
+    c.execute('''UPDATE film SET  language = 'polish', length = 97 WHERE id = 2''')
+    c.execute('''DELETE FROM film WHERE id = 1 ''')
+    conn.commit()
 
-# Wyświetlić film z kategorii thriller w języku angielskim
-c.execute('''SELECT JSON_OBJECT(
-    'id', id,
-    'name', name,
-    'category', category,
-    'length', length,
-    'language', language)
-    FROM film WHERE category LIKE 'Thriller' AND language LIKE 'english' ''')
 
-output = c.fetchall()
-print(output)
+def main():
+    conn = sqlite3.connect('Baza_pyth')
+    c = conn.cursor()
+    queries(c)
+    
+    modify(c, conn)
 
-# Zmień długość i język filmu o id=2 zgodnie z tabelą
-c.execute('''UPDATE film SET  language = 'polish', length = 97 WHERE id = 2''')
 
-# Usuń film o id=1
-c.execute('''DELETE FROM film WHERE id = 1 ''')
-
-conn.commit()
+if __name__ == "__main__":
+    main()
