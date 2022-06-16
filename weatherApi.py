0d5fa72cb6e28b8a353657a0ed2e4de7df0473a0
Rakeduk <mlodydudek@gmail.com>
2022-06-16 15:08:57 +0200

    Complete weatherApi.py and fix style

--- weatherApi.py
+++ weatherApi.py
@@ -1,10 +1,15 @@
 import requests
 import api_key
-import json
 
-towns = ["Warsaw", "London", "New York"]
 
-for town in towns:
-	response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key.weather_key}&q={town}&aqi=no")
-	weather = response.json()['current']['temp_c']
-	print(town, weather)
+def main():
+    towns = ["Warsaw", "London", "New York"]
+    for town in towns:
+        response = requests.get(
+            f"http://api.weatherapi.com/v1/current.json?key={api_key.weather_key}&q={town}&aqi=no")
+        weather = response.json()['current']['temp_c']
+        print(town, weather)
+
+
+if __name__ == "__main__":
+    main()
